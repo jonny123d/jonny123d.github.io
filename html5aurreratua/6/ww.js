a73247e99068a9be66be54dd1b17bd7ae441eb13
jonny123d <alzamendizabal@hotmail.com>
2015-11-10 13:01:57 +0100

    Update ww.js

--- html5aurreratua/6/ww.js
+++ html5aurreratua/6/ww.js
@@ -1,18 +1,23 @@
-var i = 2;
-if (n == 1 || n == 2)
+onmessage = function(n)
 {
-	postMessage(true);
-	terminate();
-}
-for (; i < n; ++i)
-{
-	if (n % 100 == 0)
-		postMessage(n);
-	if (n % i == 0)
-	{
-		postMessage(false);
-		terminate();
-	}
-}
-postMessage(true);
-terminate();
+    var i = 2;
+    if (n == 1 || n == 2)
+    {
+        postMessage(true);
+        terminate();
+    }
+    for (; i < n; ++i)
+    {
+        if (n % 100 == 0)
+            postMessage(n);
+        if (n % i == 0)
+        {
+            postMessage(false);
+            terminate();
+        }
+    }
+    postMessage(true);
+    terminate();
+};
+
+
