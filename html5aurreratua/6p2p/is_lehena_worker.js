b9c524507ba71cf6bc2db9b2ebb895fbe9f204fd
jonny123d <alzamendizabal@hotmail.com>
2015-11-17 13:37:54 +0100

    Update is_lehena_worker.js

--- html5aurreratua/6p2p/is_lehena_worker.js
+++ html5aurreratua/6p2p/is_lehena_worker.js
@@ -1,19 +1,20 @@
 
-onmessage = function(e) {
-	n=e.data;
-
-	var i = 2;
-	if (n == 2) 
-	{
-		postMessage([true, n]);
-		close();
-	}
+self.onmessage = function(event){
+	n = event.data;
+	console.log("Sartu da n: " + n);
+	
+    var i = 2;
+	if (n == 2) return true;
 	for (; i < n; ++i) {
-		if (n % i == 0) {	
-			postMessage([false, n]);
-			close();
+		if (n % i == 0) {
+			postMessage(i);
+			//document.getElementById("irteera").innerHTML += i;
+		       	return false;
 		}
+		 //console.log("Kalkulatzen:" + i);
 	}
-
-	postMessage([true, n]);
+ 	//clearInterval(erloju);	
+	//document.getElementById("irteera").innerHTML += i;
+	postMessage(i);
+	return true;
 };
