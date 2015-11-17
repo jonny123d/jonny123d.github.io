3b0cbe43c48aa89c8fa73cf07b6516d863589ff1
jonny123d <alzamendizabal@hotmail.com>
2015-11-17 13:33:02 +0100

    Update worker.js
    Dabid Martinez

--- html5aurreratua/6p2p/worker.js
+++ html5aurreratua/6p2p/worker.js
@@ -1,20 +1,19 @@
 
-self.onmessage = function(event){
-	n = event.data;
-	console.log("Sartu da n: " + n);
-	
-    var i = 2;
-	if (n == 2) return true;
-	for (; i < n; ++i) {
-		if (n % i == 0) {
-			postMessage(i);
-			//document.getElementById("irteera").innerHTML += i;
-		       	return false;
-		}
-		 //console.log("Kalkulatzen:" + i);
+onmessage = function(e) {
+	n=e.data;
+
+	var i = 2;
+	if (n == 2) 
+	{
+		postMessage([true, n]);
+		close();
 	}
- 	//clearInterval(erloju);	
-	//document.getElementById("irteera").innerHTML += i;
-	postMessage(i);
-	return true;
+	for (; i < n; ++i) {
+		if (n % i == 0) {	
+			postMessage([false, n]);
+			close();
+		}
+	}
+
+	postMessage([true, n]);
 };
